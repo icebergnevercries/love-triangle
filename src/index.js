e817f2ac7af32454ffac16bb649f1fad806db284
icebergnevercries <denislosik@mail.ru>
2018-09-21 22:34:38 +0300

    added function implementation getLoveTrianglesCount

--- src/index.js
+++ src/index.js
@@ -3,5 +3,18 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var length = preferences.length;
+  var count = 0;
+  for (var i=0;i<length;i++)
+  {
+    if(preferences[i]!=-1/*check of condition for counting different love triangles phenomenons*/
+      && preferences[i]-1<length && preferences[preferences[i]-1]<length && preferences[preferences[preferences[i]-1]-1]<length/*check the output of indexes beyond the limits of array*/
+      && preferences[i]!=preferences[preferences[i]-1] && preferences[preferences[i]-1]!=preferences[preferences[preferences[i]-1]-1]/* the elimination of counting equal values ​​in array*/
+      && preferences[preferences[preferences[i]-1]-1]==i+1/*check of main condition*/)
+    {
+      count++;//calculating love triangles phenomenons
+      preferences[i]=-1;preferences[preferences[i]-1]=-1;preferences[preferences[preferences[i]-1]-1]=-1;//the condition for counting different love triangles phenomenons
+    }    
+  }
+  return count;
 };
